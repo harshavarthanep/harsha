fa1b85570e42cb589f5f7a7ec8ab6696d42d5a12
harshavarthanep <113576943+harshavarthanep@users.noreply.github.com>
2025-01-05 21:49:39 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <section
-      id="home"
     >
     <Image src="./assets/img/hero.png" className="h-[80vh] lg:h-[912px] bg-hero bg-cover bg-center bg-no-repeat" />
       <div className="container mx-auto h-full">
