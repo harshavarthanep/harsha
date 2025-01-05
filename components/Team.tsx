791f8d8ea850d8acd39b79d8889b401854edd2f3
harshavarthanep <113576943+harshavarthanep@users.noreply.github.com>
2025-01-05 20:07:03 +0530

    Update Team.tsx

--- components/Team.tsx
+++ components/Team.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 const trainerData = [
   {
-    image: "/assets/img/trainers/david.jpg",
+    image: "public/assets/img/trainers/david.jpg",
     name: "Prasanth",
     role: "Body builder coach",
     description: "He'll push you to your limits and help you achieve your goals.",
